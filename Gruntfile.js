5c9d6a7587800d4a076742abe29b665642cf076a
Brian Woodward <brian.woodward@gmail.com>
2013-12-20 18:16:50 -0500

    setting up grunt

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    /**
+     * Lint all JavaScript
+     */
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      files: [
+        'Gruntfile.js',
+        'src/**/*.js'
+      ]
+    },
+
+    /**
+     * Run mocha tests.
+     */
+    mochaTest: {
+      tests: {
+        options: {
+          reporter: 'progress'
+        },
+        src: ['src/test/**/*_test.js']
+      }
+    },
+
+    readme: {
+      options: {
+        boilerplate: 'node-util'
+      }
+    },
+
+    watch: {
+      dev: {
+        files: ['src/**/*.js'],
+        tasks: ['default']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-readme');
+
+  // Default task.
+  grunt.registerTask('default', ['test']);
+  grunt.registerTask('test', ['mochaTest']);
+  grunt.registerTask('dev', ['default', 'watch']);
+
+};
---
+++ docs/README.tmpl.md
@@ -0,0 +1,17 @@
+# {%= name %} {%= _.badge("fury") %} {%= _.badge("travis") %}
+
+> {%= description %}
+
+## Work In Progress
+This is just a place holder with more to come shortly.
+
+## Getting Started
+{%= _.doc("quickstart.md") %}
+
+## License
+{%= copyright %}
+{%= license %}
+
+***
+
+{%= _.include("footer.md") %}
